dcd3b0e4308b2c733295f470af9151f53ae26203
Ian W <iwilson001@csbsju.edu>
2020-06-02 12:05:52 -0500

    Deleted all unused code and removed the JSON file because it was unused in this version

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,3 @@
-//var data = JSON.parse(JSON/data.js);
-
 //This is the JSON object where the graph  will receive all of its data
 var data = {"title": "Graph of Speed Over Time",
   "xaxis": "Time",
@@ -7,16 +5,6 @@ var data = {"title": "Graph of Speed Over Time",
   "xvals": [1, 2, 3, 4, 5, 6],
   "yvals": [60, 48, 33, 37, 26, 12] };
 
-
-/*
-  This was for testing purposes only and can be removed
-
-  console.log(data.title);
-  console.log(data.xaxis);
-  console.log(data.yaxis);
-  console.log(data.xvals[0]);
-  console.log(data.yvals[0]);
-*/
 var title = data.title;
 var xaxis = data.xaxis;
 var yaxis = data.yaxis;
@@ -25,35 +13,6 @@ var yvals = data.yvals;
 
 function getData() {
 
-  /*
-  This is old code to make the graph using text boxes
-
-  title = document.getElementById("title").value;
-  xaxis = document.getElementById("xaxis").value;
-  yaxis = document.getElementById("yaxis").value;
-
-  var temp = document.getElementById("xvals").value;
-  var newVal = "";
-  for(var i = 0; i < temp.length; i++){
-    while(temp.charAt(i) != ',' && i < temp.length){
-      newVal += (temp.charAt(i)).toString();
-      i++;
-    }
-    xvals.push(Number(newVal));
-    newVal = "";
-  }
-
-  temp = document.getElementById("yvals").value;
-  newVal = "";
-  for(var i = 0; i < temp.length; i++){
-    while(temp.charAt(i) != ',' && i < temp.length){
-      newVal += (temp.charAt(i)).toString();
-      i++;
-    }
-    yvals.push(Number(newVal));
-    newVal = "";
-  }
-*/
 var layout = {
 title: title,
 xaxis: {
