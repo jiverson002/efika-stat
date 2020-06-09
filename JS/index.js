4b39cb09c2afa31ba9c501e93c33a645b0e020b1
Ian W <iwilson001@csbsju.edu>
2020-06-09 10:00:05 -0500

    The commit includes a working fetch API call to a remote data.json file as well as previous code in case it would ever need to be used

--- JS/index.js
+++ JS/index.js
@@ -1,22 +1,80 @@
 //This is the JSON object where the graph  will receive all of its data
+/*
 var data = {"title": "Graph of Speed Over Time",
   "xaxis": "Time",
   "yaxis": "Speed",
-  "xvals": [1, 2, 3, 4, 5, 6],
-  "yvals": [60, 48, 33, 37, 26, 12] };
 
-  fetch( 'http://localhost:8080/JSON_Server/data.json' )
-  .then( resp => resp.json() )
-  .then( json => {console.log(json.title)});
+  "xvals":[
+    [1, 2, 3, 4, 5, 6],
+    [1, 2, 3, 4, 5, 6]
+  ],
+  "yvals":[
+    [60, 48, 33, 37, 26, 12],
+    [12, 32, 43, 83, 57, 22]
 
+  ]
+
+  };
+
+*/
+  fetch('https://gist.githubusercontent.com/iwilson001/295fce39e79e828be028489a4c3b43bd/raw/a7777a7f31f88cb625bc6f0eb836276334794b8e/data.json')
+  .then(function(response) {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    // Read the response as json.
+    return response.json();
+  })
+  .then(function(responseAsJson) {
+    // Do stuff with the JSON
+    console.log(responseAsJson);
+  })
+  .catch(function(error) {
+    console.log('Looks like there was a problem: \n', error);
+  });
+
+/*
 var title = data.title;
 var xaxis = data.xaxis;
 var yaxis = data.yaxis;
 var xvals = data.xvals;
 var yvals = data.yvals;
 
-function getData() {
 
+var getData = new Vue({
+
+e1: '#app',
+data(){
+  return{
+    info: null
+  }
+},
+mounted(){
+  axios
+  .get('https://gist.githubusercontent.com/iwilson001/295fce39e79e828be028489a4c3b43bd/raw/a7777a7f31f88cb625bc6f0eb836276334794b8e/data.json')
+  .then(response => (this.info = response.data.bpi))
+}
+
+});
+*/
+function getData() {
+  var trace1 = {
+    x: data.xvals[0],
+    y: data.yvals[0],
+    type: 'scatter'
+  };
+  var trace2 = {
+    x: data.xvals[1],
+    y: data.yvals[1],
+    type: 'scatter'
+  };
+
+var graph = [trace1, trace2];
+
+Plotly.newPlot('myDiv', graph);
+
+
+/* Old original plot
 var layout = {
 title: title,
 xaxis: {
@@ -31,6 +89,7 @@ TESTER = document.getElementById('tester');
 Plotly.newPlot(TESTER, [{
 x: xvals,
 y: yvals }], layout );
+*/
 }
 
 function resetData(){
