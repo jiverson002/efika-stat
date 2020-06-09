eb2b823489b391f32db93ca98c2941393b4b19ce
Ian W <iwilson001@csbsju.edu>
2020-06-09 15:54:25 -0500

    This allows for an infinite amount of lines to be generated on the plot based on the arrays given, also made the plot responsive to window size

--- JS/index.js
+++ JS/index.js
@@ -7,55 +7,55 @@ var yaxis = null;
 var xvals = null;
 var yvals = null;
 
-  fetch('https://gist.githubusercontent.com/iwilson001/295fce39e79e828be028489a4c3b43bd/raw/a7777a7f31f88cb625bc6f0eb836276334794b8e/data.json')
-  .then(function(response) {
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    // Read the response as json.
-    return response.json();
-  })
-  .then(function(responseAsJson) {
-    // Do stuff with the JSON
-    data = responseAsJson;
-    title = data.title;
-    xaxis = data.xaxis;
-    yaxis = data.yaxis;
-    xvals = data.xvals;
-    yvals = data.yvals;
-  })
-  .catch(function(error) {
-    console.log('Looks like there was a problem: \n', error);
-  });
+fetch('https://gist.githubusercontent.com/iwilson001/295fce39e79e828be028489a4c3b43bd/raw/a7777a7f31f88cb625bc6f0eb836276334794b8e/data.json')
+.then(function(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  // Read the response as json.
+  return response.json();
+})
+.then(function(responseAsJson) {
+  // Do stuff with the JSON
+  data = responseAsJson;
+  title = data.title;
+  xaxis = data.xaxis;
+  yaxis = data.yaxis;
+  xvals = data.xvals;
+  yvals = data.yvals;
+})
+.catch(function(error) {
+  console.log('Looks like there was a problem: \n', error);
+});
 
 
 function getData() {
-  var trace1 = {
-    x: data.xvals[0],
-    y: data.yvals[0],
-    type: 'scatter'
-  };
-  var trace2 = {
-    x: data.xvals[1],
-    y: data.yvals[1],
-    type: 'scatter'
-  };
 
-var graph = [trace1, trace2];
-
-var layout = {
-  title: title,
-  xaxis: {
-    title: xaxis,
-  },
-  yaxis: {
-    title: yaxis,
+  var graph = new Array();
+  for(var i = 0; i < data.xvals.length; i++){
+    var trace1 = {
+      x: data.xvals[i],
+      y: data.yvals[i],
+      type: 'scatter'
+    };
+    graph.push(trace1);
   }
-};
 
-Plotly.newPlot('myDiv', graph, layout);
+  var layout = {
+    title: title,
+    xaxis: {
+      title: xaxis,
+    },
+    yaxis: {
+      title: yaxis,
+    }
+  };
+
+var config = {responsive: true}
+
+  Plotly.newPlot('myDiv', graph, layout, config);
 }
 
 function resetData(){
-Plotly.purge("tester");
+  Plotly.purge("tester");
 }
