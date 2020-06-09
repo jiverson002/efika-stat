33a10e9ee1278b2fe819945a070d572086806a3e
Ian W <iwilson001@csbsju.edu>
2020-06-09 10:02:15 -0500

    Delted unnecessary lines of code and unused file to clean up this branch

--- JS/index.js
+++ JS/index.js
@@ -1,22 +1,5 @@
 //This is the JSON object where the graph  will receive all of its data
-/*
-var data = {"title": "Graph of Speed Over Time",
-  "xaxis": "Time",
-  "yaxis": "Speed",
 
-  "xvals":[
-    [1, 2, 3, 4, 5, 6],
-    [1, 2, 3, 4, 5, 6]
-  ],
-  "yvals":[
-    [60, 48, 33, 37, 26, 12],
-    [12, 32, 43, 83, 57, 22]
-
-  ]
-
-  };
-
-*/
   fetch('https://gist.githubusercontent.com/iwilson001/295fce39e79e828be028489a4c3b43bd/raw/a7777a7f31f88cb625bc6f0eb836276334794b8e/data.json')
   .then(function(response) {
     if (!response.ok) {
@@ -33,30 +16,7 @@ var data = {"title": "Graph of Speed Over Time",
     console.log('Looks like there was a problem: \n', error);
   });
 
-/*
-var title = data.title;
-var xaxis = data.xaxis;
-var yaxis = data.yaxis;
-var xvals = data.xvals;
-var yvals = data.yvals;
 
-
-var getData = new Vue({
-
-e1: '#app',
-data(){
-  return{
-    info: null
-  }
-},
-mounted(){
-  axios
-  .get('https://gist.githubusercontent.com/iwilson001/295fce39e79e828be028489a4c3b43bd/raw/a7777a7f31f88cb625bc6f0eb836276334794b8e/data.json')
-  .then(response => (this.info = response.data.bpi))
-}
-
-});
-*/
 function getData() {
   var trace1 = {
     x: data.xvals[0],
@@ -72,24 +32,6 @@ function getData() {
 var graph = [trace1, trace2];
 
 Plotly.newPlot('myDiv', graph);
-
-
-/* Old original plot
-var layout = {
-title: title,
-xaxis: {
-  title: xaxis,
-},
-yaxis: {
-  title: yaxis,
-}
-};
-
-TESTER = document.getElementById('tester');
-Plotly.newPlot(TESTER, [{
-x: xvals,
-y: yvals }], layout );
-*/
 }
 
 function resetData(){
