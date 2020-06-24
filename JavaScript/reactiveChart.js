38da1e0ad2f1a9a929e48bff7e6395b3664412ea
Ian W <iwilson001@csbsju.edu>
2020-06-24 14:50:13 -0500

    Branched off of master to new Branch called Version0, this branch is the same as branch: SeperationDesign, but for some reason, SeperationDesign refused to merge

---
+++ JavaScript/reactiveChart.js
@@ -0,0 +1,86 @@
+Vue.component("reactive-chart", {
+  props: ["url"],
+
+  template: `
+  <div>
+    <input type="text" id="JSONfile" value="Enter a Valid URL" size="100"/>
+    <button type="submit" v-on:click="createGraph">Submit</button>
+    <div :ref="chart.uuid">
+    </div>
+  </div>`,
+
+  data: function() {
+    return {
+      chart: {
+        uuid: "123",
+        traces: [],
+        layout: {}
+      }
+    }
+  },
+
+  methods: {
+    createGraph: function() {
+      fetch (document.getElementById("JSONfile").value)
+        .then(response => response.json())
+        .then(json => {
+          var layout = "";
+          var traces = "";
+          layout = {
+            title:json.title,
+            xaxis: {
+              type: 'date',
+              title: json.xaxis
+            },
+            yaxis: {
+              title: json.yaxis
+            }
+          };
+         traces = json.traces;
+          Plotly.react(
+            this.$refs[this.chart.uuid],
+            traces,
+            layout
+          );
+        })
+    }
+  },
+
+  watch: {
+    chart: {
+      handler: function() {
+        Plotly.react(
+          this.$refs[this.chart.uuid],
+          this.chart.traces,
+          this.chart.layout
+        );
+      },
+      deep: true
+    }
+  },
+
+  created() {
+    fetch (this.url)
+      .then(response => response.json())
+      .then(json => {
+        this.chart.layout = {
+          title:json.title,
+          xaxis: {
+            title: json.xaxis
+          },
+          yaxis: {
+            title: json.yaxis
+          }
+        };
+        this.chart.traces = json.traces;
+      })
+  },
+
+  mounted() {
+    Plotly.plot(
+      this.$refs[this.chart.uuid],
+      this.chart.traces,
+      this.chart.layout
+    );
+  }
+});
