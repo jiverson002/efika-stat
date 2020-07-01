60d3959d3fcfd21ee0640a69276c5e9e9c678090
Ian W <iwilson001@csbsju.edu>
2020-07-01 11:47:29 -0500

    Now working page with CSS, still glitchy with the navbar

--- JavaScript/reactiveChart.js
+++ JavaScript/reactiveChart.js
@@ -9,7 +9,8 @@ Vue.component("reactive-chart", {
   <input type="text" id="JSONfile" value="Enter a Valid URL" style="color: black; margin-right: auto;margin-left: auto; width: 100%;"/>
   </td>
   <td width="80px">
-  <button style="color: black; width: 75px;" type="submit" v-on:click="createGraph">Submit</button>
+  <button id="submitButton" style="color: black; width: 75px;" type="submit" v-on:click="createGraph">Submit</button>
+  <button id="updateButton" style="color: black; width: 75px; display: none; " type="submit" v-on:click="updateGraph">Update</button>
   </td>
   </tr>
   </table>
@@ -22,13 +23,42 @@ Vue.component("reactive-chart", {
       chart: {
         uuid: "123",
         traces: [],
-        layout: {}
+        layout: {},
+        plotted: false,
       }
     }
   },
 
   methods: {
     createGraph: function() {
+      fetch (document.getElementById("JSONfile").value)
+      .then(response => response.json())
+      .then(json => {
+        var layout = "";
+        var traces = "";
+        layout = {
+          title:json.title,
+          xaxis: {
+            type: 'date',
+            title: json.xaxis
+          },
+          yaxis: {
+            title: json.yaxis
+          }
+        };
+        var config = {responsive: true};
+        traces = json.traces;
+        Plotly.plot(
+          this.$refs[this.chart.uuid],
+          traces,
+          layout,
+          config
+        );
+      })
+      document.getElementById("updateButton").style.display="block";
+      document.getElementById("submitButton").style.display="none";
+    },
+    updateGraph: function(){
       fetch (document.getElementById("JSONfile").value)
       .then(response => response.json())
       .then(json => {
@@ -70,30 +100,12 @@ Vue.component("reactive-chart", {
     }
   },
 
-  created() {
-    fetch (this.url)
-    .then(response => response.json())
-    .then(json => {
-      this.chart.layout = {
-        title:json.title,
-        xaxis: {
-          title: json.xaxis
-        },
-        yaxis: {
-          title: json.yaxis
-        }
-      };
-      this.chart.traces = json.traces;
-      this.chart.config = {responsive: true};
-    })
-  },
-
-  mounted() {
-    Plotly.plot(
-      this.$refs[this.chart.uuid],
-      this.chart.traces,
-      this.chart.layout,
-      this.chart.config
-    );
-  }
+  // mounted() {
+  //   Plotly.plot(
+  //     this.$refs[this.chart.uuid],
+  //     this.chart.traces,
+  //     this.chart.layout,
+  //     this.chart.config
+  //   );
+  // }
 });
