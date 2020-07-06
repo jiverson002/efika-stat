392fa7a0920529cf9f60817119a16cc6f144b95d
Ian W <iwilson001@csbsju.edu>
2020-07-06 09:19:05 -0500

    Fixed the function overlap, now there is only one function

--- JavaScript/reactiveChart.js
+++ JavaScript/reactiveChart.js
@@ -9,8 +9,8 @@ Vue.component("reactive-chart", {
   <input type="text" id="JSONfile" value="Enter a Valid URL" style="color: black; margin-right: auto;margin-left: auto; width: 100%;"/>
   </td>
   <td width="80px">
-  <button id="submitButton" style="color: black; width: 75px;" type="submit" v-on:click="createGraph">Submit</button>
-  <button id="updateButton" style="color: black; width: 75px; display: none; " type="submit" v-on:click="updateGraph">Update</button>
+  <button id="submitButton" style="color: black; width: 75px;" type="submit" v-on:click="createOrUpdateGraph">Submit</button>
+  <button id="updateButton" style="color: black; width: 75px; display: none; " type="submit" v-on:click="createOrUpdateGraph">Update</button>
   </td>
   </tr>
   </table>
@@ -23,14 +23,13 @@ Vue.component("reactive-chart", {
       chart: {
         uuid: "123",
         traces: [],
-        layout: {},
-        plotted: false,
+        layout: {}
       }
     }
   },
 
   methods: {
-    createGraph: function() {
+    createOrUpdateGraph: function() {
       fetch (document.getElementById("JSONfile").value)
       .then(response => response.json())
       .then(json => {
@@ -48,40 +47,24 @@ Vue.component("reactive-chart", {
         };
         var config = {responsive: true};
         traces = json.traces;
-        Plotly.plot(
-          this.$refs[this.chart.uuid],
-          traces,
-          layout,
-          config
-        );
-      })
-      document.getElementById("updateButton").style.display="block";
-      document.getElementById("submitButton").style.display="none";
-    },
-    updateGraph: function(){
-      fetch (document.getElementById("JSONfile").value)
-      .then(response => response.json())
-      .then(json => {
-        var layout = "";
-        var traces = "";
-        layout = {
-          title:json.title,
-          xaxis: {
-            type: 'date',
-            title: json.xaxis
-          },
-          yaxis: {
-            title: json.yaxis
-          }
-        };
-        var config = {responsive: true};
-        traces = json.traces;
-        Plotly.react(
-          this.$refs[this.chart.uuid],
-          traces,
-          layout,
-          config
-        );
+        if(document.getElementById("updateButton").style.display == "none"){
+          Plotly.plot(
+            this.$refs[this.chart.uuid],
+            traces,
+            layout,
+            config
+          );
+          document.getElementById("updateButton").style.display="block";
+          document.getElementById("submitButton").style.display="none";
+        }
+        else{
+          Plotly.update(
+            this.$refs[this.chart.uuid],
+            traces,
+            layout,
+            config
+          );
+        }
       })
     }
   },
