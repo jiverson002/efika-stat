fa907560b57a499d4a7e2a9948e30cad8bbfe826
Ian W <iwilson001@csbsju.edu>
2020-08-18 12:20:24 -0500

    Reformatted tooling script to remove code and increase readability

--- Tooling/index.py
+++ Tooling/index.py
@@ -1,34 +1,48 @@
 import sys
+import datetime
 
 '''print ("The script has the name %s" % (sys.argv[1]))'''
 
-dataStart = 127
-dataEnd = 142
-#name of input/output file on cmd line arg ***
-dataFile = "perf3.out"
+#Variables from cmd line
+title = sys.argv[1]
+xaxisTitle = sys.argv[2]
+yaxisTitle = sys.argv[3]
+dataFile = sys.argv[4]
+fileToPrintTo = sys.argv[5]
+
+#Start of columns with data in them
+dataStart = 127 # +18 every loop to get to next set of data
+dataEnd = 142 # +18 every loop to get to next set of data
 
 # "x" to create, "a" to append, "w" to write
-file = open("data.json", "w")
+file = open(fileToPrintTo, "w")
+
 # Start of the JSON file
 file.write("{\n")
+
 # Start of the layout of the JSON file
 file.write("\"layout\": {\n")
+
 # Title of the graph
 file.write("\"title\": \"")
-file.write(sys.argv[1] + "\",\n")
+file.write(title + "\",\n")
+
 # Formatting and title for the x-axis
 file.write("\"xaxis\": {\n")
 file.write("\"type\": \"Date\",\n")
 file.write("\"title\": \"")
-file.write(sys.argv[2] + "\" \n")
+file.write(xaxisTitle + "\" \n")
 file.write("},\n")
+
 # Formatting and title for the y axis
 file.write("\"yaxis\": {\n")
 file.write("\"title\": \"")
-file.write(sys.argv[3] + "\" \n")
+file.write(yaxisTitle + "\" \n")
 file.write("}\n")
+
 # End of the Layout section
 file.write("},\n")
+
 # Start of the Trace section
 file.write("\"traces\": [\n")
 i = 0
@@ -39,37 +53,12 @@ while i < 6:
     xvals = []
     f = open(dataFile, "r")
     for x in f:
-        #datetime obj ***
         if "@date:" in x:
-            # x[11:31]
-            data = x[27:31]
-            if "Jan" in x:
-                data = data + "-01"
-            elif "Feb" in x:
-                data = data + "-02"
-            elif "Mar" in x:
-                data = data + "-03"
-            elif "Apr" in x:
-                data = data + "-04"
-            elif "May" in x:
-                data = data + "-05"
-            elif "Jun" in x:
-                data = data + "-06"
-            elif "Jul" in x:
-                data = data + "-07"
-            elif "Aug" in x:
-                data = data + "-08"
-            elif "Sep" in x:
-                data = data + "-09"
-            elif "Oct" in x:
-                data = data + "-10"
-            elif "Nov" in x:
-                data = data + "-11"
-            else:
-                data = data + "-12"
-            data = data + "-" + x[15:18] + " " + x[18:27]
-            xvals.append(data)
-    f.close()
+            # x[11:31], this does not include the timezone,
+            # this converts data to datetime and prints out to datafile in proper form
+            my_date = datetime.datetime.strptime(x[7:31], "%c")
+            xvals.append(my_date.strftime("%Y") + "-" + my_date.strftime("%m") + "-" + my_date.strftime("%d") + " " + my_date.strftime("%X"))
+    f.seek(0)
     for x in xvals:
         if x == xvals[len(xvals) - 1]:
             file.write("\"" + x + "\"")
@@ -81,14 +70,12 @@ while i < 6:
     # Start of yvals for first data set
     file.write("\"y\":[")
     yvals = []
-    f = open(dataFile, "r")
     for x in f:
         x = x[dataStart:dataEnd].replace(" ", "")
         test = x
         if test.replace('.', '', 1).isdigit():
             yvals.append(x)
-    #f.rewind() ***
-    f.close()
+    f.seek(0)
     index = 0
     for x in yvals:
         if index == len(yvals) - 1:
@@ -101,20 +88,20 @@ while i < 6:
 
     # last touches of trace including: name, type, and text for each plot
     file.write("\"name\":\"")
-    f = open(dataFile, "r")
     for x in f:
         x = x[dataStart:dataEnd]
         if isinstance(x, str) and x != "":
             file.write(x)
             break
-    f.close()
+    f.seek(0)
     file.write("\",\n")
     # end of title
+
     file.write("\"type\":\"scatter\",\n")
     # end of type
+
     hashes = []
     file.write("\"text\": [")
-    f = open(dataFile, "r")
     for x in f:
         if "@commit" in x:
             hashes.append(x[8:48])
@@ -128,6 +115,7 @@ while i < 6:
     dataStart += 18
     dataEnd += 18
     # end of text for each data point
+
     if i == 5:
         file.write("}\n")
     else:
