71fdb5fbc9a11e3d3576676cf12d3472d3cad093
Jeremy Iverson <jiverson002@csbsju.edu>
2020-08-19 14:27:29 -0500

    app: Updates app to final version from Ian
    This commits includes all changes made by @iwilson001 during the summer
    of 2020. This includes the responsive plotting web application, as well
    as a Python tool to parse data from a tabular format to a JSON format
    appropriate for use with the web app.

--- JavaScript/reactiveChart.js
+++ JavaScript/reactiveChart.js
@@ -3,10 +3,19 @@ Vue.component("reactive-chart", {
 
   template: `
   <div>
-    <input type="text" id="JSONfile" value="Enter a Valid URL" size="100"/>
-    <button type="submit" v-on:click="createGraph">Submit</button>
-    <div :ref="chart.uuid">
-    </div>
+  <table width="100%">
+  <tr>
+  <td width="100%">
+  <input type="text" id="JSONfile" value="Enter a Valid URL" style="color: black; margin-right: auto;margin-left: auto; width: 100%;"/>
+  </td>
+  <td width="80px">
+  <button id="submitButton" style="color: black; width: 75px;" type="submit" v-on:click="createOrUpdateGraph">Submit</button>
+  <button id="updateButton" style="color: black; width: 75px; display: none; " type="submit" v-on:click="createOrUpdateGraph">Update</button>
+  </td>
+  </tr>
+  </table>
+  <div :ref="chart.uuid">
+  </div>
   </div>`,
 
   data: function() {
@@ -18,69 +27,50 @@ Vue.component("reactive-chart", {
       }
     }
   },
-
+//Creates the chart or updates it depending on which button above is active
   methods: {
-    createGraph: function() {
+    createOrUpdateGraph: function() {
       fetch (document.getElementById("JSONfile").value)
-        .then(response => response.json())
-        .then(json => {
-          var layout = "";
-          var traces = "";
-          layout = {
-            title:json.title,
-            xaxis: {
-              type: 'date',
-              title: json.xaxis
-            },
-            yaxis: {
-              title: json.yaxis
-            }
-          };
-         traces = json.traces;
+      .then(response => response.json())
+      .then(json => {
+        var layout = "";
+        var traces = "";
+        layout = json.layout;
+        var config = {responsive: true};
+        traces = json.traces;
+        if(document.getElementById("updateButton").style.display == "none"){
+          Plotly.plot(
+            this.$refs[this.chart.uuid],
+            traces,
+            layout,
+            config
+          );
+          document.getElementById("updateButton").style.display="block";
+          document.getElementById("submitButton").style.display="none";
+        }
+        else{
           Plotly.react(
             this.$refs[this.chart.uuid],
             traces,
-            layout
+            layout,
+            config
           );
-        })
+        }
+      })
     }
   },
-
+//watches for changes to the charts current data
   watch: {
     chart: {
       handler: function() {
         Plotly.react(
           this.$refs[this.chart.uuid],
           this.chart.traces,
-          this.chart.layout
+          this.chart.layout,
+          this.chart.config
         );
       },
       deep: true
     }
   },
-
-  created() {
-    fetch (this.url)
-      .then(response => response.json())
-      .then(json => {
-        this.chart.layout = {
-          title:json.title,
-          xaxis: {
-            title: json.xaxis
-          },
-          yaxis: {
-            title: json.yaxis
-          }
-        };
-        this.chart.traces = json.traces;
-      })
-  },
-
-  mounted() {
-    Plotly.plot(
-      this.$refs[this.chart.uuid],
-      this.chart.traces,
-      this.chart.layout
-    );
-  }
 });
---
+++ Tooling/index.py
@@ -0,0 +1,133 @@
+import sys
+import datetime
+
+'''print ("The script has the name %s" % (sys.argv[1]))'''
+
+#Variables from cmd line
+title = sys.argv[1]
+xaxisTitle = sys.argv[2]
+yaxisTitle = sys.argv[3]
+dataFile = sys.argv[4]
+fileToPrintTo = sys.argv[5]
+
+#Start of columns with data in them
+dataStart = 127 # +18 every loop to get to next set of data
+dataEnd = 142 # +18 every loop to get to next set of data
+
+# "x" to create, "a" to append, "w" to write
+file = open(fileToPrintTo, "w")
+
+# Start of the JSON file
+file.write("{\n")
+
+# Start of the layout of the JSON file
+file.write("\"layout\": {\n")
+
+# Title of the graph
+file.write("\"title\": \"")
+file.write(title + "\",\n")
+
+# Formatting and title for the x-axis
+file.write("\"xaxis\": {\n")
+file.write("\"type\": \"Date\",\n")
+file.write("\"tickangle\": \"15\",\n")
+file.write("\"nticks\": \"8\",\n")
+file.write("\"title\": \"")
+file.write(xaxisTitle + "\" \n")
+file.write("},\n")
+
+# Formatting and title for the y axis
+file.write("\"yaxis\": {\n")
+file.write("\"title\": \"")
+file.write(yaxisTitle + "\" \n")
+file.write("}\n")
+
+# End of the Layout section
+file.write("},\n")
+
+# Start of the Trace section
+file.write("\"traces\": [\n")
+i = 0
+while i < 6:
+    # Start of the first trace
+    file.write("{\n")
+    file.write("\"x\":[")
+    xvals = []
+    f = open(dataFile, "r")
+    for x in f:
+        if "@date:" in x:
+            # x[11:31], this does not include the timezone, yyyy-mm-dd hh:mm:ss
+            # this converts data to datetime and prints out to datafile in proper form
+            my_date = datetime.datetime.strptime(x[7:31], "%c")
+            xvals.append(my_date.strftime("%Y-%m-%d %X")) #+ "-" + my_date.strftime("%m") + "-" + my_date.strftime("%d") + " " + my_date.strftime("%X")
+    f.seek(0)
+    for x in xvals:
+        if x == xvals[len(xvals) - 1]:
+            file.write("\"" + x + "\"")
+        else:
+            file.write("\"" + x + "\"" + ",")
+    file.write("],\n")
+    # end of xvals for first dataset
+
+    # Start of yvals for first data set
+    file.write("\"y\":[")
+    yvals = []
+    for x in f:
+        x = x[dataStart:dataEnd].replace(" ", "")
+        test = x
+        if test.replace('.', '', 1).isdigit():
+            yvals.append(x)
+    f.seek(0)
+    index = 0
+    for x in yvals:
+        if index == len(yvals) - 1:
+            file.write("\"" + x + "\"")
+        else:
+            file.write("\"" + x + "\"" + ",")
+        index += 1
+    file.write("],\n")
+    # end of yvals for first data set
+
+    # last touches of trace including: name, type, and text for each plot
+    file.write("\"name\":\"")
+    for x in f:
+        x = x[dataStart:dataEnd]
+        if isinstance(x, str) and x != "":
+            file.write(x)
+            break
+    f.seek(0)
+    file.write("\",\n")
+    # end of title
+
+    file.write("\"type\":\"scatter\",\n")
+    # end of type
+
+    hashes = []
+    file.write("\"text\": [")
+    for x in f:
+        if "@commit" in x:
+            hashes.append(x[8:48])
+    f.close()
+    for x in hashes:
+        if x == hashes[len(hashes) - 1]:
+            file.write("\"" + x + "\"")
+        else:
+            file.write("\"" + x + "\"" + ",")
+    file.write("]\n")
+    dataStart += 18
+    dataEnd += 18
+    # end of text for each data point
+
+    if i == 5:
+        file.write("}\n")
+    else:
+        file.write("},\n")
+    # end of trace object
+    i += 1
+
+file.write("]\n")
+# end of trace array
+
+file.write("}\n")
+file.close()
+# end of file
